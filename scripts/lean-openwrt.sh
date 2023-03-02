70eb27739d478b25e7315881b96ac364f72c3212
cyh92 <cyh92@live.cn>
2023-03-02 21:52:48 +0800

    1

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -90,12 +90,12 @@ TEMP=$(date +"%Y%m%d_%H%M%S")
 sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
 sed -i "s/OpenWrt /v${TEMP}\/ /g" package/lean/default-settings/files/zzz-default-settings
 # 删除自定义源默认的 argon 主题
-rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
 # 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
-find ./ -name luci-theme-argon | xargs rm -rf;
+#find ./ -name luci-theme-argon | xargs rm -rf;
 #拉取 argon的源码
-git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
 
 #git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #全能推送
-git clone https://github.com/cyh92/luci-app-pushbot package/luci-app-pushbot
+#git clone https://github.com/cyh92/luci-app-pushbot package/luci-app-pushbot
