9477d5293eea9d75c014cf4f03801a010e163218
cyh92 <cyh92@live.cn>
2023-03-05 21:03:31 +0800

    1

--- scripts/offical-openwrt.sh
+++ scripts/offical-openwrt.sh
@@ -125,11 +125,11 @@ sed -i "s/192.168.1.1/192.168.50.5/g" package/base-files/files/bin/config_genera
 date=`date +%Y.%m.%d`
 sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
 # 删除自定义源默认的 argon 主题
-# rm -rf package/lean/luci-theme-argon
-# # 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
-# find ./ -name luci-theme-argon | xargs rm -rf;
-# #拉取 argon的源码
-# git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf;
+#拉取 argon的源码
+git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
 
 # #git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # #全能推送
