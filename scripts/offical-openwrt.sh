11b12d5a864eca439b0baf7a18a4b333b54e3c1c
cyh92 <cyh92@live.cn>
2023-03-06 21:44:13 +0800

    1

--- scripts/offical-openwrt.sh
+++ scripts/offical-openwrt.sh
@@ -66,7 +66,7 @@ echo "设备IP参数为：$1";
 # cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 # # Add luci-theme-argon
-# git clone -b 19.07_stable --depth=1 https://github.com/jerrykuku/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 
 # # Add smartdns
 # svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
@@ -130,8 +130,8 @@ rm -rf package/lean/luci-theme-argon
 # 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
 find ./ -name luci-theme-argon | xargs rm -rf;
 #拉取 argon的源码
-git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
+# git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
 
-# #git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # #全能推送
 # git clone https://github.com/cyh92/luci-app-pushbot package/luci-app-pushbot
--- scripts/project-openwrt.sh
+++ scripts/project-openwrt.sh
@@ -20,8 +20,8 @@ rm -rf package/lean/luci-theme-argon
 # 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
 find ./ -name luci-theme-argon | xargs rm -rf;
 #拉取 argon的源码
-git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
+# git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
 
-#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # #全能推送
 # git clone https://github.com/cyh92/luci-app-pushbot package/luci-app-pushbot
