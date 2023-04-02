dbbe0e42acba2cd377b3b251af921e09b65cdb06
cyh92 <cyh92@live.cn>
2023-04-02 11:26:16 +0800

    1

--- scripts/project-openwrt.sh
+++ scripts/project-openwrt.sh
@@ -28,12 +28,11 @@ find ./ -name luci-theme-argon | xargs rm -rf;
 # git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
 
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#全能推送
-#git clone https://github.com/cyh92/luci-app-pushbot package/luci-app-pushbot
-
-#git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
 
 #支持OpenWrt官方源码22.03版本luci-app-zerotier
 find ./ -name luci-app-zerotier | xargs rm -rf;
 git clone https://github.com/lylus/luci-app-zerotier package/luci-app-zerotier
+
+
+git clone https://github.com/SuLingGG/default-settings package/default-settings
 
