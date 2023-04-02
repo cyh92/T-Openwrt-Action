5967629f2cb9c948ed72f559b4178616b97919ef
cyh92 <cyh92@live.cn>
2023-04-02 12:23:38 +0800

    1

--- ipq40xx/scripts/custom.sh
+++ ipq40xx/scripts/custom.sh
@@ -9,8 +9,16 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # Add date version
-export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
-sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
+# export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
+# sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
+#设置版本为当前时间
+date=`date +%Y.%m.%d`
+sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
+
+sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION='R18.06'" >> package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='$date'" >> package/base-files/files/etc/openwrt_release
 
 # Fix mt76 wireless driver
 pushd package/kernel/mt76
