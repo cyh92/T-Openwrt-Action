021f806090d9c1ff14d72b042d651232fa4a8680
cyh92 <cyh92@live.cn>
2023-04-21 19:33:55 +0800

    Update custom.sh

--- ipq40xx/scripts/custom.sh
+++ ipq40xx/scripts/custom.sh
@@ -43,12 +43,12 @@ rm -rf package/lean/luci-theme-argon
 # 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
 find ./ -name luci-theme-argon | xargs rm -rf;
 #拉取 argon的源码
-git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
 
-#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 #支持OpenWrt官方源码22.03版本luci-app-zerotier
-#find ./ -name luci-app-zerotier | xargs rm -rf;
-#git clone https://github.com/lylus/luci-app-zerotier package/luci-app-zerotier
+find ./ -name luci-app-zerotier | xargs rm -rf;
+git clone https://github.com/lylus/luci-app-zerotier package/luci-app-zerotier
 
 
