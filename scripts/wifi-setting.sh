68f362f35cbabca70701901d32cc644861d06f37
cyh92 <cyh92@live.cn>
2024-05-21 09:20:56 +0800

    Update wifi-setting.sh

--- scripts/wifi-setting.sh
+++ scripts/wifi-setting.sh
@@ -3,4 +3,4 @@
 # This script is only for Raspberry Pi 4, Thanks @xmapst
 
 # sed -i 's/36/44/g;s/VHT80/VHT40/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
