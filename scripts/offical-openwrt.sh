5d30bfaed59e1911ba4dd369b882b6afceae790c
cyh92 <cyh92@live.cn>
2024-05-22 12:21:08 +0800

    Update offical-openwrt.sh

--- scripts/offical-openwrt.sh
+++ scripts/offical-openwrt.sh
@@ -9,6 +9,9 @@
 sed -i "s/192.168.1.1/$1/g" package/base-files/files/bin/config_generate
 echo "设备IP参数为：$1";
 #cat package/base-files/files/bin/config_generate
+
+cat package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+
 # Clone Lean's latest sources.
 # pushd package
 # git clone --depth=1 https://github.com/coolsnowwolf/lede
