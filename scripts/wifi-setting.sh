01ff9670993761a0ca27fef1ff8d5bb1b44831da
cyh92 <cyh92@live.cn>
2024-05-22 12:09:44 +0800

    Update wifi-setting.sh

--- scripts/wifi-setting.sh
+++ scripts/wifi-setting.sh
@@ -4,3 +4,5 @@
 
 # sed -i 's/36/44/g;s/VHT80/VHT40/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+
+cat package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
